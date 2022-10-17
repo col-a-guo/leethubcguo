d4bafb60eeb38a4fa289b70e82d128133365e048
Collin A Guo <bunjuku8@gmail.com>
2022-10-17 06:27:38 -0700

    Time: 679 ms (98.86%), Space: 14.4 MB (35.07%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,44 @@
+#questions
+#negative coins (i.e. change) isn't allowed, right?
+#
+#general idea
+#we are essentially transversing a large graph with a sink and implied edges
+#so use a shortest path algorithm
+#memoize, add lowest, build up and check each coin for prior options
+#figure out LCM for coin pairs?
+#
+#
+class Solution:
+    def coinChange(self, coins, amount: int) -> int:
+        if amount == 0:
+            return 0
+        memo = [10000000 for i in range(amount+1)]
+        i = 0
+        while i < len(coins):
+            if coins[i] <= amount:
+                memo[coins[i]] = 1
+                i += 1
+            else:
+                coins.pop(i)
+        
+                
+        if coins == []:
+            return -1
+        for i in range(min(coins)+1, amount+1):
+            memo[i] = self.coinChangeMemo(memo, coins, i)
+            
+        print(memo[amount-20:amount+1])
+        if memo[amount] == 10000000:
+            return -1
+        else:
+            return memo[amount]
+        
+    def coinChangeMemo(self, memo, coins, index):
+        best = memo[index]
+        for coin in coins:
+            if index >= coin:
+                guess = memo[index-coin]
+                if guess < best:
+                    best = guess+1
+        return best
+                
